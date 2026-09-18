130f84ca998039c8386a79537e24fd4352aedac3
Mafe1324 <mariaf.huertasm@uqvirtual.edu.co>
2026-09-17 23:43:28 -0500

    ms-academia

---
+++ src/main/java/com/pontebella/ms_academia/MsAcademiaApplication.java
@@ -0,0 +1,13 @@
+package com.pontebella.ms_academia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAcademiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAcademiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pontebella/ms_academia/MsAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pontebella.ms_academia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
